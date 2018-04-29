8c1733fab411940ffcf64952eee9377cfae80726
maks <maksimdimitrov@bunte-bloecke.de>
2018-04-29 19:49:28 +0200

    * Node now reads index.html

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var http = require('http');
+var fs = require('fs');
+// var mainScript = require('./mainScript');
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+var server = http.createServer(function (request, response) {
+  fs.readFile('index.html', function(err, data) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+  });
 });
 
 var port = process.env.PORT || 1337;
