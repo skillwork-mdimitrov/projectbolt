add0a11b22f37dd98cc593d4f6f2355f66158c1b
maks <maksimdimitrov@bunte-bloecke.de>
2018-04-30 00:42:39 +0200

    * Dumb test x2

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ var server = http.createServer(function (request, response) {
     // Handle HTML
     if(request.url === "/") {
       fs.readFile('index.html', function(err, data) {
+        if(err){
+          console.log("HTML failed to load");
+        }
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
         response.end();
@@ -19,6 +22,9 @@ var server = http.createServer(function (request, response) {
     // Handle scripts
     if(request.url === "/mainScript.js") {
       fs.readFile('mainScript.js', function(err, data) {
+        if(err){
+          console.log("Scripts failed to load");
+        }
         response.writeHead(200, {'Content-Type': 'application/javascript'});
         response.write(data);
         response.end();
@@ -27,16 +33,20 @@ var server = http.createServer(function (request, response) {
     // Handle images
     if(request.url === "/images/thunder.png") {
       fs.readFile('images/thunder.png', function(err, data) {
-        if(!err){
+        if(err){
+          console.log("Images failed to load");
+        }
           response.writeHead(200, {'Content-Type': 'image/png'});
           response.write(data);
           response.end();
-        }
       });
     }
     // Handle CSS
     if(request.url === "/styles/style_index.css") {
       fs.readFile("styles/style_index.css", function(err, data) {
+        if(err) {
+          console.log("Loading styles failed");
+        }
         response.writeHead(200, {'Content-Type': 'text/css'});
         response.write(data);
         response.end();
