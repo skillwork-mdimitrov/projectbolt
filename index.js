3fd2702760318073307c267d8f8cfa79483de457
maks <maksimdimitrov@bunte-bloecke.de>
2018-04-30 12:21:50 +0200

    * Port changed to 443

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ var server = http.createServer(function (request, response) {
 /* PORT
    ============================================================== */
 // Specify port
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 443;
 // Listen to port
 server.listen(port);
 // =====================================================================================================================
