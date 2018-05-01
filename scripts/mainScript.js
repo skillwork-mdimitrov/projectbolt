23e6678e10bf08a23172da80a13dff9c2ab8fa90
maks <maksimdimitrov@bunte-bloecke.de>
2018-05-01 17:45:29 +0200

    * AJAX on request failure logging

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -18,16 +18,22 @@ $(document).ready(function() {
     // global.searchedQuery.html("<em>The search query is ... </em>" + global.searchField.val());
 
     // Send AJAX request on searchBtn click
-    $.ajax({url: "scripts/sqltest/sqltest.js", success: function(result){ // HC URL + exposed server architecture (will fix later)
-      // Cover all JS scenarios for falsy results
-      if(result === 'undefined' || typeof result === 'undefined' || result === null) {
-        global.searchedQuery.html("Couldn't fetch results from the database");
+    $.ajax({url: "scripts/sqltest/sqltest.js",
+      success: function(result){ // HC URL + exposed server architecture (will fix later)
+        // Cover all JS scenarios for falsy results
+        if(result === 'undefined' || typeof result === 'undefined' || result === null) {
+          global.searchedQuery.html("Couldn't fetch results from the database");
+        }
+        // Correct results, display them
+        else {
+          global.searchedQuery.html(result);
+        }
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        console.log("Status: " + textStatus); alert("Error: " + errorThrown);
+        global.searchedQuery.html("Bad request");
       }
-      // Correct results, display them
-      else {
-        global.searchedQuery.html(result);
-      }
-    }});
+      });
   })
 });
 
