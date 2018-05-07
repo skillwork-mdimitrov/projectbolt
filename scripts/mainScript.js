862839a511412b88af8d2747f2875515a06ec87f
maks <maksimdimitrov@bunte-bloecke.de>
2018-05-07 17:50:49 +0200

    mend
    *Vanilla JS AJAX -> jQuery AJAX; Minor code cleanup

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -24,27 +24,19 @@ var scriptPromise = new Promise(function(resolve, reject) {
 // Fetch the whole database
 function fetchDB() {
   "use strict";
-  try {
-    // VARIABLES
-    var xhttp;
-    xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function() {
-      var DONE = 4; // readyState 4 means the request is done.
-      var OK = 200; // status 200 is a successful return.
-      if (this.readyState === DONE && this.status === OK) {
-        // the results from this request will be stored in the questions variable.
-        // since the results coming from the AJAX request are as string, split by coma first and then store in array
-        global.questions = this.responseText.split(",");
-        outsideResolve(global.questions);
-      }
-    };
-    xhttp.open("POST", "dynamic_request_fetchDB", true);
-    xhttp.send();
-  }
-  catch(e) {
-    console.log('Caught Exception: ' + e.message);
-  }
+    $.ajax({
+    type: "POST",
+    url: "dynamic_request_fetchDB",
+    success: function(result){
+      // since the results coming from the AJAX request are as string, split by coma first and then store in array
+      global.questions = result.split(",");
+      // the results from this request will be stored in the questions variable.
+      outsideResolve(global.questions);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      console.log("Error caught, status: " + textStatus + " error: " + errorThrown);
+    }
+  });
 }
 
 // When everything has loaded
@@ -53,16 +45,10 @@ $(document).ready(function() {
   /* ATTACH EVENT LISTENERS
     ============================================================== */
   global.searchInput.on("input", function() {
-    // sendRequestSQL();
     fetchDB(); // send a request that fetches the db rows
     scriptPromise.then(function(resolve) {
       evaluateQuery(resolve);
     });
-
-    // // hacky async ... wait 2 seconds (so the results had for sure arrived and then display them)
-    // setTimeout(function() {
-    //   evaluateQuery(global.questions);
-    // }, 2000);
   });
 });
 
