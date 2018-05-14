a5db7e38ad2859fc21c465be1adeef000f99aec8
Jasper <smitj3961@gmail.com>
2018-05-14 12:05:28 +0200

    Insert ratings

--- public/scripts/rateAnswer.js
+++ public/scripts/rateAnswer.js
@@ -1,19 +1,3 @@
-var answers = [
-    "Yes",
-    "No",
-    "May be",
-    "Ok"
-]
-
-var users = [
-    "Hank",
-    "Bob",
-    "Alice",
-    "Jerry",
-    "John",
-    "Diane"
-]
-
 function loadAnswers()
 {
     "use strict";
@@ -26,7 +10,6 @@ function loadAnswers()
         // vq.questions = data.split(",");
         // vq.outsideResolve(vq.questions);
         $.each(data, function (index, value) {
-            console.log(data);
             $('#answerSelector').append($('<option>', { 
                 value: value["id"],
                 text : value["answer"] 
@@ -84,20 +67,9 @@ $(document).ready(function(){
     });
 
     loadAnswers();
-    loadUsers();
-    $.each(answers, function (index, value) {
-        $('#answerSelector').append($('<option>', { 
-            value: index,
-            text : value 
-        }));         
-    });
-    
-    $.each(users, function (index, value) {
-        $('#userSelector').append($('<option>', { 
-            value: index,
-            text : value 
-        }));         
-    });
+    setTimeout(function() {
+        loadUsers();
+    }, 75);    
 });
 
 $(".rating").change(function() {
@@ -112,26 +84,22 @@ function rateAnswer()
 
     if (answerID !== null && userID !== null && rating > 0)
     {
-        // "use strict";
-        // $.ajax({
-        //     type: 'post',
-        //     data: {"answer" : answerID, "user": userID, "rating": rating},
-        //     url: 'dynamic_request_writeToDB',
-        //     success: function(data){
-        //     console.log("data written");
-        //     },
-        //     error: function(jqXHR, textStatus, errorThrown) {
-        //         alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
-        //         console.log('jqXHR: ' + jqXHR);
-        //         console.log('textStatus: ' + textStatus);
-        //         console.log('errorThrown: ' + errorThrown);
-        //     }
-        // });
-
-        query = "INSERT INTO ratings (answerid, userid, rating) VALUES (" + answerID + ", " + userID + ", " + rating + ")"
-        console.log("Running query:");
-        console.log(query);
-        console.log("Resetting fields");
+        "use strict";
+        $.ajax({
+            type: 'post',
+            data: {"answer" : answerID, "user": userID, "rating": rating},
+            url: 'request_writing_rating_todb',
+            success: function(data){
+            console.log("data written");
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
+                console.log('jqXHR: ' + jqXHR);
+                console.log('textStatus: ' + textStatus);
+                console.log('errorThrown: ' + errorThrown);
+            }
+        });
+        
         $('#answerSelector option:first').prop('selected',true);
         $('#userSelector option:first').prop('selected',true);
         $('#ratingSelector').rating('clear rating');
