6bf92e079b868aa9bff3304c074c0e4bfa7db427
Jasper <smitj3961@gmail.com>
2018-05-16 09:20:14 +0200

    Input validation

--- routes/rating.js
+++ routes/rating.js
@@ -30,16 +30,24 @@ router.get('/get-all-users', function(req, res, next) {
 
 /* GET all the users */
 router.get('/insert-rating/:answerID/:userID/:rating', function(req, res, next) {
-    database.runGenericQuery("INSERT INTO Ratings (AnswerID, UserID, Rating) VALUES (" + 
+    if (!isNaN(req.params["answerID"]) && !isNaN(req.params["userID"]) &&!isNaN(req.params["rating"]))
+    {
+        database.runGenericQuery("INSERT INTO Ratings (AnswerID, UserID, Rating) VALUES (" + 
                                                                     req.params["answerID"] + ", " 
                                                                     + req.params["userID"] + ", " 
                                                                     + req.params["rating"] 
                                                                     + ")").then(() => {
-        res.status(200).send("Insert succesful");
-    }).catch((reason) => {
-        console.log('Handle rejected promise ('+reason+') here.');
-        res.status(500).send('Something broke! ' + reason)
-    });  
+            res.status(200).send("Insert succesful");
+        }).catch((reason) => {
+            console.log('Handle rejected promise ('+reason+') here.');
+            res.status(500).send('Something broke! ' + reason)
+        }); 
+    }
+    else
+    {
+        console.log('Illegal input');
+        res.status(500).send('Something broke! Illegal input')
+    }
 });
 
 module.exports = router;
