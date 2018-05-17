92ce4de463f374b9e2faded219d4073657922ad6
maks <maksimdimitrov@bunte-bloecke.de>
2018-05-17 12:32:16 +0200

    * NAMESPACED addAnswer

--- public/scripts/addAnswer.js
+++ public/scripts/addAnswer.js
@@ -1,57 +1,57 @@
-/* addAnswers NAMESPACE
+/* addAnswer NAMESPACE
  ============================================================== */
-const aa = function() {
-  let answerButton = $('#answerButton');
-  let answerBox = $('#answerBox');
-  let questionIDBox = $('#questionIDBox');
+const addAnswer = function() {
+  const answerButton = $('#answerButton');
+  const answerBox = $('#answerBox');
+  const questionIDBox = $('#questionIDBox');
+  const postAnswer = function (bodyJSON){
+    "use strict";
+    $.ajax({
+      type: 'POST',
+      data: bodyJSON,
+      url: 'add-answer',
+      success: function(data){
+        alert('Answer added successfully for question ' + addAnswer.questionIDBox.val());
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
+        console.log('jqXHR: ' + jqXHR);
+        console.log('textStatus: ' + textStatus);
+        console.log('errorThrown: ' + errorThrown);
+      }
+    });
+  };
+  const fieldsValidation = function() {
+    if(addAnswer.answerBox.val().length > 0 && addAnswer.questionIDBox.val().length > 0) {
+      return true;
+    }
+  };
 
-  // aa namespace will reveal the following properties
+  // addAnswer namespace will reveal the following properties
   return {
     answerButton: answerButton,
     answerBox: answerBox,
-    questionIDBox: questionIDBox
+    questionIDBox: questionIDBox,
+    postAnswer: postAnswer,
+    fieldsValidation: fieldsValidation
   }
 }();
-
-function postAnswer(bodyJSON){
-  "use strict";
-  $.ajax({
-    type: 'POST',
-    data: bodyJSON,
-    url: 'add-answer',
-    success: function(data){
-      alert('Answer added successfully for question ' + aa.questionIDBox.val());
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
-      console.log('jqXHR: ' + jqXHR);
-      console.log('textStatus: ' + textStatus);
-      console.log('errorThrown: ' + errorThrown);
-    }
-  });
-}
-
-function fieldsValidation() {
-  if(aa.answerBox.val().length > 0 && aa.questionIDBox.val().length > 0) {
-    return true;
-  }
-}
 
 // When everything has loaded
 $(document).ready(function() {
   "use strict";
   /* ATTACH EVENT LISTENERS
     ============================================================== */
-  aa.answerButton.on("click", function() {
-    if(fieldsValidation()) {
+  addAnswer.answerButton.on("click", function() {
+    if(addAnswer.fieldsValidation()) {
       // JSON'ize the answer and questionID
       let bodyJSON = {
-        questionID: aa.questionIDBox.val(),
-        answer: aa.answerBox.val()
+        questionID: addAnswer.questionIDBox.val(),
+        answer: addAnswer.answerBox.val()
       };
 
       // Send the AJAX request
-      postAnswer(bodyJSON);
+      addAnswer.postAnswer(bodyJSON);
     }
     else {
       alert('Please write an answer and question id in the appropriate fields')
