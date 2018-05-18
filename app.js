bf8738e29ebe53abca7bc82ef3dd871165b916a5
Jasper <smitj3961@gmail.com>
2018-05-18 19:46:09 +0200

    Basic chat (client side only)

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var ratingRouter = require('./routes/rating');
 var addQuestionRouter = require('./routes/addQuestion');
 var addAnswerRouter = require('./routes/addAnswer');
 var loginRouter = require('./routes/login');
+var chatRouter = require('./routes/chat');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/rating', ratingRouter);
 app.use('/add-question', addQuestionRouter);
 app.use('/add-answer', addAnswerRouter);
 app.use('/login', loginRouter);
+app.use('/chat', chatRouter);
 
 // Make this script publicly available
 module.exports = app;
