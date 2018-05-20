f1d12f5892f007c5b50803586c6635de33329d2e
maks <maksimdimitrov@bunte-bloecke.de>
2018-05-20 16:57:05 +0200

    * View answers folding bar [to be made globally available] * Major CSS/HTML changes in viewAnswers/viewQuestions

--- public/scripts/viewAnswers.js
+++ public/scripts/viewAnswers.js
@@ -7,6 +7,8 @@ const viewAnswers = function() {
     const addAnswerArea = $('#addAnswerArea');
     const submitAnswerBtn = $('#submitAnswerBtn');
     const cancelAnswerBtn = $('#cancelAnswerBtn');
+    let foldingHeader; // will be dynamically assigned value
+    let headerInfo; // will be dynamically assigned value
 
     const addToTable = function(answer) {
         let answerText = answer[0];
@@ -144,6 +146,40 @@ const viewAnswers = function() {
       return questionID;
     };
 
+    // [THIS WILL BE FURTHER ABSTRACTED AND PORTED TO A SEPARATE SCRIPT FOR EVERYBODY TO USE]
+    // GREEN or RED folding bar
+    const unfoldHeader = function(textToDisplay, green) {
+      // Create a folding header if it doesn't exists
+      if($("#foldingHeader").length === 0) {
+
+        // Created, append and select the folding header
+        $("body").prepend("<div id='foldingHeader' class='foldingHeader'></div>");
+        foldingHeader = $('#foldingHeader'); // select it
+
+        // Create, append and select the text in the folding header
+        foldingHeader.append("<p id='headerInfo' class='headerInfo'></p>");
+        headerInfo = $('#headerInfo'); // select it
+      }
+
+      // Check whether the method is expecting green or red coloured folding header
+      if(green === true) {
+        foldingHeader.css("background-color", "rgba(2, 237, 112, 0.7)") // green
+      }
+      else {
+        foldingHeader.css("background-color", "rgba(139, 0, 0, 0.7)") // red
+      }
+
+      // Unfold the header
+      foldingHeader.css("height", "50px");
+      // Write the folding header text
+      headerInfo.text(textToDisplay);
+
+      // Hide the header after 5 seconds
+      setTimeout(function() {
+        foldingHeader.css("height", "0");
+      }, 5000)
+    };
+
     // AJAX post answer
     const postAnswer = function (bodyJSON){
       "use strict";
@@ -152,7 +188,7 @@ const viewAnswers = function() {
         data: bodyJSON,
         url: 'add-answer',
         success: function(data){
-          alert('Answer added successfully for question ' + viewAnswers.getQuestionID());
+          unfoldHeader("Answer added successfully", true);
         },
         error: function(jqXHR, textStatus, errorThrown) {
           alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
@@ -184,6 +220,7 @@ const viewAnswers = function() {
         updateRatings: updateRatings,
         addOwnAnswer: addOwnAnswer,
         getQuestionID: getQuestionID,
+        unfoldHeader: unfoldHeader,
         postAnswer: postAnswer,
         fieldNotEmpty: fieldNotEmpty
     }
@@ -211,7 +248,7 @@ $(document).ready(function() {
         viewAnswers.addAnswerArea.val(''); // Reset textarea
       }
       else {
-        alert("Please fill in an answer");
+        viewAnswers.unfoldHeader("Please fill in an answer", false);
       }
     });
 
