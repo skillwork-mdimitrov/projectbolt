5a92280c6f221eb677d60e6bfc74c273c357189a
maks <maksimdimitrov@bunte-bloecke.de>
2018-05-22 23:54:50 +0200

    * Global function remove element from an array by value

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -7,18 +7,38 @@ const global = function() {
   /* @return {true} if the field is NOT empty
   ============================================================== */
   const fieldNotEmpty = function(field) {
+    "use strict";
     return field.val().length > 0;
   };
 
   /* @return {true} if the field is IS empty
   ============================================================== */
   const fieldIsEmpty = function(field) {
+    "use strict";
     return field.val().length === 0;
+  };
+
+  /* Remove element from array by value
+  * const myArr = ["one", "two", "three", "four"];
+  * rmElemFromArray(myArr, "two");
+  ============================================================== */
+  const rmElemFromArray = function remove(array, element) {
+    "use strict";
+    const value = array.indexOf(element);
+
+    if (value !== -1) {
+      array.splice(value, 1);
+      return true;
+    }
+    else {
+      return false;
+    }
   };
 
 return {
   fieldNotEmpty: fieldNotEmpty,
-  fieldIsEmpty: fieldIsEmpty
+  fieldIsEmpty: fieldIsEmpty,
+  rmElemFromArray: rmElemFromArray
 }
 }();
 //  ============================================================== */
