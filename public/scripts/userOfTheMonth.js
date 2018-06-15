406ab0b90daf1e408ac37d60f3bee50c82427fc7
Jasper <smitj3961@gmail.com>
2018-06-15 14:12:54 +0200

    User of the month in namespace

--- public/scripts/userOfTheMonth.js
+++ public/scripts/userOfTheMonth.js
@@ -1,55 +1,57 @@
-const userOfTheMonth = function () 
-{
-	if (!Date.prototype.toSQLString) 
-	{
-		//Run function if the default doesn't exist
-		(
-			function() 
-			{
-				//Padding out the month / date / hours and minutes
-				function pad(number) 
+const userOfTheMonth = function () {
+	const getUserOfTheMonth = function() {
+		if (!Date.prototype.toSQLString) 
+		{
+			//Run function if the default doesn't exist
+			(
+				function() 
 				{
-					if (number < 10) 
+					//Padding out the month / date / hours and minutes
+					function pad(number) 
 					{
-						return '0' + number;
+						if (number < 10) 
+						{
+							return '0' + number;
+						}
+						return number;
 					}
-					return number;
-				}
-				//generating output string
-				Date.prototype.toSQLOString = function() 
-				{
-					return pad(this.getUTCMonth() + 1) +
-						'-' + pad(this.getUTCDate()) +
-						'-' + this.getUTCFullYear() +
-						' ' + pad(this.getUTCHours()) +
-						':' + pad(this.getUTCMinutes());
-				};
-			}()
-		);
+					//generating output string
+					Date.prototype.toSQLOString = function() 
+					{
+						return pad(this.getUTCMonth() + 1) +
+							'-' + pad(this.getUTCDate()) +
+							'-' + this.getUTCFullYear() +
+							' ' + pad(this.getUTCHours()) +
+							':' + pad(this.getUTCMinutes());
+					};
+				}()
+			);
+		}
+		//save date
+		var currentDate = new Date();
+		var currentMonth = currentDate.getMonth() + 1;
+		if (currentMonth == 13)
+		{
+			currentMonth - 1;
+		} 
+		var lastmonth = currentMonth - 1;
+		if (lastmonth == 0)
+		{
+			lastmonth = 1;
+		}
+		var monthEndDay = new Date(currentDate.getYear, lastmonth + 1, 0);
+		let monthStart = (new Date(currentDate.getYear, lastmonth, 1, 00, 00, 00)).toSQLOString();
+		let monthEnd = (new Date(currentDate.getYear, lastmonth, monthEndDay, 00, 00, 00)).toSQLOString();
+		let ansernumberUser = $.post("answer/user-answer-number", monthStart, monthEndDay);
+		ansernumberUser.then((userPostedAnsers) => {
+			console.out(userPostedAnsers);
+		}).catch(() => {
+			console.log("SOMETHING WENT WRONG!!!!");
+		});
+	};	
+
+	return {
+		getUserOfTheMonth: getUserOfTheMonth
 	}
-	//save date
-	var currentDate = new Date();
-	var currentMonth = currentDate.getMonth() + 1;
-	if (currentMonth == 13)
-	{
-		currentMonth - 1;
-	} 
-	var lastmonth = currentMonth - 1;
-	if (lastmonth == 0)
-	{
-		lastmonth = 1;
-	}
-	var monthEndDay = new Date(currentDate.getYear, lastmonth + 1, 0);
-	let monthStart = (new Date(currentDate.getYear, lastmonth, 1, 00, 00, 00)).toSQLOString();
-	let monthEnd = (new Date(currentDate.getYear, lastmonth, monthEndDay, 00, 00, 00)).toSQLOString();
-	let ansernumberUser = $.post("answer/user-answer-number", monthStart, monthEndDay);
-	ansernumberUser.then((userPostedAnsers) => {
-		console.out(userPostedAnsers[0].UserID);
-		return;
-	});            
-    catch(() => {
-	
-	});
-}
-();
+}();
 
